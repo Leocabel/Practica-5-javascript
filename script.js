625d53f3d49c73f9adcd90db58a423fcdf86c723
Leonardo Casas <leocabel@hotmail.com>
2024-05-10 21:15:23 -0500

    agregar declaracion de variables

---
+++ script.js
@@ -0,0 +1,37 @@
+/*agregar comentario: alt + shift+ a */
+/*comentario varias lineas */
+//comentar una linea
+
+
+
+
+
+console.log("Hola Mundo")
+console.log("aqui podemos escribir cualquier cosa")
+console.log("adios")
+console.log("no hay autocompletado")
+console.log(12345)
+console.log(-1.123)
+
+// Tipos de datos
+
+
+/*vamos a declarar una variable */
+let nombre = "Sebastian"
+console.log(nombre)
+nombre = "juan"
+console.log(nombre)
+
+
+/*declara una constante */
+const pi = 3.1416
+console.log(pi)
+
+let numero1 = 7
+let numero2 = 9
+let resultado = numero1 + numero2
+console.log("resultado:", resultado)
+
+
+
+
