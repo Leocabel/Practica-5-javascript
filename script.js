f161786cc2f6cacde2c3c3a966282df60dd66a2d
sebastian arias <jsebas.ariasd@gmail.com>
2024-05-14 20:53:27 -0500

    practica Java Script

--- script.js
+++ script.js
@@ -32,6 +32,65 @@ let numero2 = 9
 let resultado = numero1 + numero2
 console.log("resultado:", resultado, "algo")
 
+// Tipos de datos
 
+// strings - texto
+const ciudad = "bogota"
+const pais1 = "colombia"
+const pais2 = "argentina"
+console.log(ciudad, pais1, pais2)
+
+// lenguaje debilmente tipado
+// int, float - numeros
+const suma = 5
+const edad = 20.5
+const numeroNegativo = "-10"
+
+console.log("multiplicacion:" , 5 *  "5")
+console.log(suma, edad, numeroNegativo)
+
+// Booleandos - verdadero o falso
+let esVerde = true
+let esMorado = false
+
+// -------Estructuras de Datos--------------
+
+// Array - Vectores - Listas
+
+let listaDeNumeros = [20, -55, 100]
+console.log(listaDeNumeros)
+
+// ver un elemento de la lista
+console.log(listaDeNumeros[0])
+console.log(listaDeNumeros[1])
+console.log(listaDeNumeros[2])
+
+let listaCiudades = ["bogota" , "medellin" , "Cali"]
+
+console.log(listaCiudades)
+
+let listaDeListas = [listaDeNumeros, listaCiudades]
+console.log(listaDeListas)
+
+// acceder al dato "medellin"
+console.log(listaDeListas[1][1])
+
+// JSON (JavaScrit Objet Notation) - Objetos
+// clave - valor
+let usuario = {
+    nombre: "leonardo" , 
+    edad: 18, 
+    ciudad: "bogota" ,
+    ubicacion: {
+         latitud: 1.5356564543,
+         longitud: -2.3432353
+    }
+}
+console.log(usuario)
+console.log(usuario.nombre)
+console.log(usuario.ubicacion.latitud)
+
+usuario.edad = 25
+console.log(usuario)
 
 
